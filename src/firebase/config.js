05c6d6a3bc083d7e0a7f6b487e047cb880c979d0
Enrico Garcia <enricogarcia589@gmail.com>
2026-01-29 19:26:39 -0300

    atualizado

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,9 +2,6 @@ import firebase from "firebase/app"
 import 'firebase/auth'
 
 if (!firebase.apps.length) {
-    console.log('API Key:', process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
-    console.log('Auth Domain:', process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
-    console.log('Project ID:', process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
     firebase.initializeApp({
             apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
             authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
